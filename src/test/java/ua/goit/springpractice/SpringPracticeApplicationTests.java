77b375abeaf9d8afa4f5dcecb824f5c9bc313d69
Sergiy-Chernuha <sergiy.chernuha@gmail.com>
2023-09-19 13:41:29 +0300

    start HW 16

--- src/test/java/ua/goit/springpractice/SpringPracticeApplicationTests.java
+++ src/test/java/ua/goit/springpractice/SpringPracticeApplicationTests.java
@@ -3,11 +3,14 @@ package ua.goit.springpractice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class SpringPracticeApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void someTest() {
+		assertEquals(1,1);
 	}
 
 }
